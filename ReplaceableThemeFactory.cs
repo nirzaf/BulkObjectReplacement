1b45b04c4012ad6e45b568d60808aa8137152260
nirzaf <mfmfazrin1986@gmail.com>
2022-01-13 15:34:12 +0530

    Gatwick College is conducting professional courses in marketing, accounting and project. The college is preparing students for international professional exams.

--- ReplaceableThemeFactory.cs
+++ ReplaceableThemeFactory.cs
@@ -2,28 +2,28 @@
 
 public class ReplaceableThemeFactory
 {
-    private readonly List<WeakReference<Ref<ITheme>>> themes
+    private readonly List<WeakReference<Ref<ITheme>>> _themes
         = new();
 
-    private ITheme createThemeImpl(bool dark)
+    private ITheme CreateThemeImpl(bool dark)
     {
         return dark ? new DarkTheme() : new LightTheme();
     }
 
     public Ref<ITheme> CreateTheme(bool dark)
     {
-        var r = new Ref<ITheme>(createThemeImpl(dark));
-        themes.Add(new(r));
+        var r = new Ref<ITheme>(CreateThemeImpl(dark));
+        _themes.Add(new WeakReference<Ref<ITheme>>(r));
         return r;
     }
 
     public void ReplaceTheme(bool dark)
     {
-        foreach (var wr in themes)
+        foreach (var wr in _themes)
         {
             if (wr.TryGetTarget(out var reference))
             {
-                reference.Value = createThemeImpl(dark);
+                reference.Value = CreateThemeImpl(dark);
             }
         }
     }
